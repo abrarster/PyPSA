e51ea482554de68f7a216787d600070ba96b3e6b
Abrar <abrar.ahmed.2010@gmail.com>
2019-02-17 15:47:29 -0600

    change default solver to cbc

--- pypsa/opf.py
+++ pypsa/opf.py
@@ -1502,7 +1502,7 @@ def network_lopf_solve(network, snapshots=None, formulation="angles", solver_opt
 
     return status, termination_condition
 
-def network_lopf(network, snapshots=None, solver_name="glpk", solver_io=None,
+def network_lopf(network, snapshots=None, solver_name="cbc", solver_io=None,
                  skip_pre=False, extra_functionality=None, solver_logfile=None, solver_options={},
                  keep_files=False, formulation="angles", ptdf_tolerance=0.,
                  free_memory={},extra_postprocessing=None):
